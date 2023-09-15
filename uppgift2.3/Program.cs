02f6684eea69c843ce91a17a049a9b82dad46b01
emsj06001 <emsj06001@E-PW024NWP>
2023-09-15 12:38:27 +0200

    Add project files.

---
+++ uppgift2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_2_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra bilen?...");
+            String x = Console.ReadLine();
+            Console.WriteLine("Hur många kilometer vill du köra med bilen?...");   
+            String y = Console.ReadLine();
+
+            int dagar = int.Parse(x);
+            int km = int.Parse(y);
+            int sum = dagar * 500 + km * 1 + 300;
+            Console.WriteLine("Det kommer kosta "+ sum +" kr att hyra bilen.");
+            Console.ReadKey();
+        }
+    }
+}
